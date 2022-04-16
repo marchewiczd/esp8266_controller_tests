46f7e97f3ca751550f1f3cfd1a4eab03c724b566
Dominik Marchewicz <marchewiczd@gmail.com>
2022-04-16 14:14:41 +0200

    fixed tests for new serial commands handler

--- SerialHandlerTests/EspCommandTests.cs
+++ SerialHandlerTests/EspCommandTests.cs
@@ -7,48 +7,44 @@ namespace ESP8266_Controller_tests.SerialHandlerTests
     [Order(999)]
     public class EspCommandTests : SerialHandlerTestBase
     {
-        [Test, Order(1)]
-        public void Esp_UnrecognizedCommandArgument()
-        {
-            string testCommand = "esp badArg";
-            string[] expectedResponse = { $"Argument \"{testCommand.Split(" ")[1]}\" is not recognized for \"esp\" command.\r", $"> {testCommand}\r" };
-
-            TestCommand(testCommand, expectedResponse, 2);
-        }
-
-        [Test, Order(1)]
-        public void Esp_UnrecognizedCommandArgument_MultipleArguments()
-        {
-            string testCommand = "esp badArg1 badArg2 badArg3";
-            string[] expectedResponse = { $"Argument \"{testCommand.Split(" ")[1]}\" is not recognized for \"esp\" command.\r", $"> {testCommand}\r" };
-
-            TestCommand(testCommand, expectedResponse, 2);
-        }
-
         [Test, Order(999)]
         public void Esp_Restart()
         {
             int readDelayMs = 500;
+
             string testCommand = "esp restart";
             string[] expectedResponse =
             {
-                "~ld\r",
-                "v0004f7c0\r",
-                "csum 0xc9\r",
-                "chksum 0xc9\r",
-                "tail 4\r",
-                "load 0x3fff20b8, len 40, room 4 \r",
-                "chksum 0xcc\r",
-                "tail 4\r",
-                "load 0x4010f000, len 3460, room 16 \r",
+                "> esp restart\r",
+                "Restarting...\r",
                 "\r",
                 " ets Jan  8 2013,rst cause:2, boot mode:(3,6)\r",
                 "\r",
-                "Restarting...\r",
-                "> esp restart\r"
+                "load 0x4010f000, len 3460, room 16 \r",
+                "tail 4\r",
+                "chksum 0xcc\r",
+                "load 0x3fff20b8, len 40, room 4 \r",
+                "tail 4\r",
+                "chksum 0xc9\r",
+                "csum 0xc9\r",
+                string.Empty, //skip as it's different every time firmware changes
+                "~ld\r"
             };
 
-            TestCommand(testCommand, expectedResponse, 14, readDelayMs);
+            WriteSerial(testCommand);
+
+            Thread.Sleep(readDelayMs);
+            var actualResponse = GetSerialResponses(expectedResponse.Length);
+
+            Assert.AreEqual(expectedResponse.Length, actualResponse.Length, "Expected response and actual response lenghts are not equal");
+
+            for (int i = 0; i < expectedResponse.Length; i++)
+            {
+                if (expectedResponse[i] == string.Empty)
+                    continue;
+
+                Assert.AreEqual(expectedResponse[i], actualResponse[i]);
+            }
         }
     }
 }
--- SerialHandlerTests/GeneralCommandTests.cs
+++ SerialHandlerTests/GeneralCommandTests.cs
@@ -11,18 +11,18 @@ namespace ESP8266_Controller_tests.SerialHandlerTests
         public void Help_PrintsAllCommands()
         {
             string testCommand = "help";
-            string[] expectedResponse = { "\trestart - restarts ESP\r", "ESP\r", "> help\r" };
+            string[] expectedResponse = { "> help\r", "Available commands:\r", "\tesp restart - restart ESP\r", "", "\thelp - print this message\r" };
 
-            TestCommand(testCommand, expectedResponse, 3);
+            TestCommand(testCommand, expectedResponse, expectedResponse.Length);
         }
 
         [Test]
         public void UnrecognizedCommand()
         {
             string testCommand = "commandNotFound";
-            string[] expectedResponse = { $"Command \"{testCommand}\" is not recognized.\r", $"> {testCommand}\r" };
+            string[] expectedResponse = { $"Command \"{testCommand}\" is not recognized. Type help for available commands.\r", $"> {testCommand}\r" };
     
-                TestCommand(testCommand, expectedResponse, 2);
+                TestCommand(testCommand, expectedResponse, expectedResponse.Length);
         }
     }
 }
